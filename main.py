e1abca4720948330747ef786b646844807ec43f6
Arilu <ariwayne@proton.me>
2026-03-02 21:30:27 -0500

    Creando usuarios

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from modelos.libro import Libro
+from modelos.usuario import Usuario
 
+# Creando libros
 lib1 = Libro("100 años de soledad", "Gabriel García Márquez")
 lib2 = Libro("Drácula", "Bram Stoker")
 lib3 = Libro("Frankstein", "Mary Shelley")
@@ -10,3 +12,12 @@ lib7 = Libro("Matilda", "Roald Dahl")
 lib8 = Libro("1984", "George Orwell")
 lib9 = Libro("Rebelión en la granja", "George Orwell")
 lib10 = Libro("La llamada de Cthulhu", "H. P. Lovecraft")
+
+# Creando usuarios
+usu1 = Usuario("Ariel", "contraseña123")
+usu2 = Usuario("Victoria", "contra123")
+usu3 = Usuario("José", "12345679")
+usu4 = Usuario("Miguel", "987654321")
+usu5 = Usuario("María", "secreto123")
+usu6 = Usuario("Carla", "S_.12ñ")
+usu7 = Usuario("María", "123seña123")
