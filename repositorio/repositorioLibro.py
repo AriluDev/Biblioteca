3bcdd9c56d3e9d6a2c0dd851848810d1af788455
Arilu <ariwayne@proton.me>
2026-03-05 19:09:40 -0500

    Avanzando repositorioLibro

--- repositorio/repositorioLibro.py
+++ repositorio/repositorioLibro.py
@@ -1,2 +1,42 @@
+from modelos.libro import Libro
+
 class RepositorioLibro:
-    pass
+    def __init__(self):
+        self.__libros = []
+
+    def agregarLibro(self, libro: Libro):
+        for i in self.__libros:
+            if i.titulo == libro.titulo:
+                raise Exception("El libro ya ha sido agregado anteriormente.")
+        self.__libros.append(libro)
+
+    def eliminarLibro(self, libro: Libro):
+        for i in self.__libros:
+            if i.titulo == libro.titulo:
+                self.__libros.remove(i)
+                return
+        raise Exception("No existe el libro que se pretende eliminar.")
+
+    def buscarPorAutor(self, autor):
+        coincidencias = []
+        for libro in self.__libros:
+            if libro.autor == autor:
+                coincidencias.append(libro)
+        if len(coincidencias) > 0:
+            return coincidencias
+        raise Exception("No se encontró ningún libro del autor buscado.")
+
+    def buscarPorTitulo(self, titulo):
+        coincidencias = []
+        for libro in self.__libros:
+            if libro.titulo == titulo:
+                coincidencias.append(libro)
+        if len(coincidencias) > 0:
+            return coincidencias
+        raise Exception("No se encontró ningún libro con el título buscado.")
+
+    def listarLibros(self):
+        pass
+
+    def listarLibrosDisponibles(self):
+        pass
